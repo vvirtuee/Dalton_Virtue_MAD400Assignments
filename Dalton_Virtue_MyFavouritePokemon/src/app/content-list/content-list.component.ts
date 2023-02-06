e2c57e185d6dc6156a3e4a74a05b33276d5a36df
scvirtued <114676663+scvirtued@users.noreply.github.com>
2023-02-06 23:29:38 +0000

    Completed Step 8 (update ContentList's typescript to allow retrieval of array data)

--- Dalton_Virtue_MyFavouritePokemon/src/app/content-list/content-list.component.ts
+++ Dalton_Virtue_MyFavouritePokemon/src/app/content-list/content-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IContent } from '../models/icontent';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
-export class ContentListComponent {
+export class ContentListComponent{
+
+  pokemonArray: IContent[] = [];
+  //adding service using dependency injection
+  constructor(private service: PokemonService){}
+
+  //use service to retrieve array of data
+  ngOnInit(): void {
+    this.service.getContent().subscribe((IContentArrayOfData: IContent[]) => {
+      this.pokemonArray = IContentArrayOfData;
+    });
+  }
 }
