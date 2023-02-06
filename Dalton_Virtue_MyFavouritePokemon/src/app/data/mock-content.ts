bb04e92d29bf1ca0d09c988179e24693441c6c85
scvirtued <114676663+scvirtued@users.noreply.github.com>
2023-02-06 22:38:17 +0000

    Step 1 - 4

---
+++ Dalton_Virtue_MyFavouritePokemon/src/app/data/mock-content.ts
@@ -0,0 +1,38 @@
+export const CONTENT = [
+    {
+        id: 0,
+        name: "Voltorb",
+        description: "Electric Pokeball pokemon",
+        author: "Game Freak",
+        imgSrc: "voltorb.jpg",
+        type: "Electric",
+        tags: ["pokemon","ball"]
+    },
+    {
+        id: 1,
+        name: "Gastly",
+        description: "Ghost ball pokemon gaseous",
+        author: "Game Freak",
+        imgSrc: "gastly.png",
+        type: "Ghost",
+        tags: ["pokemon","ghost"]
+    },
+    {
+        id: 0,
+        name: "Marowak",
+        description: "Adult form of Cubone",
+        author: "Game Freak",
+        imgSrc: "marowak.png",
+        type: "Normal",
+        tags: ["bone"]
+    },
+    {
+        id: 0,
+        name: "Psyduck",
+        description: "Yellow duckling pokemon with insomnia",
+        author: "Game Freak",
+        imgSrc: "psyduck.png",
+        type: "Water",
+        tags: ["water","psychic","pokemon"]
+    }
+];
