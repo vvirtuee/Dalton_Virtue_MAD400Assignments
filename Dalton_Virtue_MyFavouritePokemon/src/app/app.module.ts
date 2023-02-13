2120b7a76692148351cba6bc910d22e2ca0ee9ef
scvirtued <114676663+scvirtued@users.noreply.github.com>
2023-02-13 22:13:47 +0000

    Assignment 4: Completed steps 1 - 4

--- Dalton_Virtue_MyFavouritePokemon/src/app/app.module.ts
+++ Dalton_Virtue_MyFavouritePokemon/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentListItemComponent } from './content-list-item/content-list-item.component';
+import { ContentDetailComponent } from './content-detail/content-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
     ContentListItemComponent,
+    ContentDetailComponent,
     
   ],
   imports: [
