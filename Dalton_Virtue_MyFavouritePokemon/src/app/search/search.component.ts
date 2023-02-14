ce1cf7b704a7cc40d98eae33e1febd5b63d5c90b
scvirtued <114676663+scvirtued@users.noreply.github.com>
2023-02-13 20:21:06 -0500

    Attempted step 11

--- Dalton_Virtue_MyFavouritePokemon/src/app/search/search.component.ts
+++ Dalton_Virtue_MyFavouritePokemon/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+  id?: number;
+  testID?: number;
+  service: PokemonService;
 
+  constructor(service: PokemonService) {
+    this.id = -1;
+    this.testID = 0;
+    this.service = service;
+    // this.inputElement = document.querySelector("input");
+
+  }
+
+  onClick(inputTagValue: string): void {
+    console.log('The value of the input is: ', inputTagValue);
+    this.testID = parseInt(inputTagValue);
+    var item = this.service.getContentItem(this.testID);
+    console.log("item: " + item);
+  }
 }
