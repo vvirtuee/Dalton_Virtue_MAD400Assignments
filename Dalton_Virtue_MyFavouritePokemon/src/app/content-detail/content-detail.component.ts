dd8a3d829c55427224186429d52c01256de982c2
scvirtued <114676663+scvirtued@users.noreply.github.com>
2023-02-13 21:05:55 -0500

    Completed step 14

--- Dalton_Virtue_MyFavouritePokemon/src/app/content-detail/content-detail.component.ts
+++ Dalton_Virtue_MyFavouritePokemon/src/app/content-detail/content-detail.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { IContent } from '../models/icontent';
+
 
 @Component({
   selector: 'app-content-detail',
   templateUrl: './content-detail.component.html',
   styleUrls: ['./content-detail.component.scss']
 })
-export class ContentDetailComponent {
+export class ContentDetailComponent implements OnInit{
+  contentItem?: IContent;
+  constructor(
+    private router: Router,
+    private contentService: ContentService,
+    private route: ActivatedRoute) {
+  }
 
+  ngOnInit():void{
+
+    //retrieve the IContent item from content service based on id passed into component
+    //via routing rule created
+    this.route.paramMap.subscribe(params => {
+      let id: number = 0;
+      
+      //concat param from url
+      id = +(params.get('id'));
+
+      this.contentService.getContentItem(id).subscribe((singleDetailItem: IContent) => {
+
+        if (singleDetailItem) {
+          this.contentItem = singleDetailItem;
+        }
+        else {
+          console.log("Item not found. Redirecting...");
+          
+          //angular redirect to 404
+          this.router.navigate(["/404"]);
+        }
+      });
+    });
+  }
+  
 }
