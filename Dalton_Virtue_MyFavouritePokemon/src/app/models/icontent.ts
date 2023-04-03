f2fac80ecb3b502925d07b03a4fd66c5f40f68f8
scvirtued <114676663+scvirtued@users.noreply.github.com>
2023-04-03 19:54:13 +0000

    14.1 & 14.2: Added header and necessary inputs for each field of IContent. also modified IContent structure for 1 parameter.

--- Dalton_Virtue_MyFavouritePokemon/src/app/models/icontent.ts
+++ Dalton_Virtue_MyFavouritePokemon/src/app/models/icontent.ts
@@ -3,7 +3,7 @@ export interface IContent {
   id?: number;  //modified IContent to accept undefined id values
   title: string;
   description?: string;
-  author: string;
+  author?: string;  //decided to make this optional since Game Freak is consistent with them all.
   imgSrc?: string;
   type: string;
   tags?: string[];
--- Dalton_Virtue_MyFavouritePokemon/src/app/modify-content/modify-content.component.ts
+++ Dalton_Virtue_MyFavouritePokemon/src/app/modify-content/modify-content.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IContent } from '../models/icontent';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-modify-content',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./modify-content.component.scss']
 })
 export class ModifyContentComponent {
-
+  pokemonToAdd: IContent = {
+    title: "",
+    description: "",
+    imgSrc: "",
+    type: "",
+  }
+  constructor(private content: PokemonService) {}
+  addContentToServer(): void {
+    console.log(this.pokemonToAdd);
+    this.content.addContent(this.pokemonToAdd).subscribe(pokemonFromServer => {
+      this.pokemonToAdd = pokemonFromServer;
+    })
+  }
 }
