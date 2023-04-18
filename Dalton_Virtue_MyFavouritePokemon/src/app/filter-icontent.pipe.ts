4f72908ad4a3f3fa10f4909c0a7ab65396f6579a
scvirtued <114676663+scvirtued@users.noreply.github.com>
2023-04-17 22:19:52 -0400

    8. ~ transform method modified to return version of IContent that has type equal to string passed into pipe.

--- Dalton_Virtue_MyFavouritePokemon/src/app/filter-icontent.pipe.ts
+++ Dalton_Virtue_MyFavouritePokemon/src/app/filter-icontent.pipe.ts
@@ -6,10 +6,12 @@ import { IContent } from 'src/app/models/icontent';
 })
 export class FilterIContentPipe implements PipeTransform {
 
+  //The method should return a filtered version of the input array of IContent, only
+  //returning IContent that has its type equal to a string passed to the pipe.
   transform(pokemons?: IContent[]): IContent[] {
     return pokemons?.filter(p => {
-      return p.tags?.length;
-    }) ?? [];
+      return p.type?.length;
+    }) ?? pokemons;
   }
 
 }
